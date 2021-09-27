7f97062096fbc87885995e61408e049d6d371b5b
harrytwright <haroldtomwright@gmail.com>
2021-09-27 14:23:53 +0100

    tests: check types
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

---
+++ test/index.types.ts
@@ -0,0 +1,45 @@
+(async () => {
+	const monk = require('monk')
+	const CLSContext  = require('zipkin-context-cls')
+	const { Tracer, ExplicitContext, ConsoleRecorder, BatchRecorder, jsonEncoder: { JSON_V2 } } = require('zipkin')
+
+	const { HttpLogger } = require('zipkin-transport-http')
+
+	const tracer = new Tracer({
+		ctxImpl: new CLSContext('namespace', true), // implicit in-process context
+		recorder: new BatchRecorder({
+			logger: new HttpLogger({
+				endpoint: 'http://localhost:9411/api/v2/spans',
+				jsonEncoder: JSON_V2
+			})
+		}), // batched http recorder
+		localServiceName: 'default' // name of this application
+	});
+
+	/**
+	 * @type {monk.IMonkManager}
+	 * */
+	const client = monk('localhost:27017/demo')
+
+	const middleware = require('../src/index')({ tracer, verbose: true })
+	client.addMiddleware(middleware)
+
+	const data = { name: 'tester' }
+	const connection = client.get(require('nanoid').nanoid())
+
+	await connection.insert(data)
+
+	// @ts-ignore
+	await connection.findOne(data._id)
+
+	// For the HTTP logger
+	await timeout(3000)
+
+	await client.close()
+})()
+
+function timeout (time: number) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, time)
+	})
+}
